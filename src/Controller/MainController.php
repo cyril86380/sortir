2fbd2c19282e7b86c07044db7c573ada893bec86
cyril86380 <99645370+cyril86380@users.noreply.github.com>
2022-04-04 14:42:24 +0200

    créer les pages de bases pour les series

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -20,6 +20,14 @@ class MainController extends AbstractController
      */
     public function test()
     {
-       return $this->render('main/test.html.twig');
+        $serie = [
+            "title" => "Game of Thrones",
+          "year" => 2000,
+        ];
+
+        return $this->render('main/test.html.twig', [
+            "mySerie" => $serie,
+            "autrevar" => 41222,
+        ]);
     }
 }
